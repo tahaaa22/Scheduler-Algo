7c7545345ce13e6dcfbc24b27237edee87f9ec24
o-shawky <130479412+o-shawky@users.noreply.github.com>
2023-04-11 17:39:33 +0200

    Update Processor.h

--- Ph1/Project1/Processor.h
+++ Ph1/Project1/Processor.h
@@ -6,7 +6,12 @@ using namespace std;
 class Processor 
 {
 private:
-    Process* CurrRun;      //new
+    Process* CurrRun;
+    char type;
+    int RDY_Length;        //increasses when new process is added and decreases during run or steal etc.
+    int BusyCount;     //incremented every time step during running
+    int Total_CPUtime;  //increases when new process is added to rdy queue
+    int Total_TRT;      //increases every time process is moved to the terminate list 
     public:
     Queue<Process*> blockedProcesses; 
     Queue<Process*> terminatedProcesses; 
@@ -14,10 +19,20 @@ private:
     Processor() //new
     {
         CurrRun = nullptr;
+        RDY_Length = 0;
+        BusyCount = 0;
+        Total_CPUtime = 0;
+        Total_TRT = 0;
 
     }
-    virtual void addtoBLK(Process*& process) = 0;
-    virtual void addtoterminate(Process* & process) = 0;
+    virtual void addtoBLK(Process*& process){
+          if (getCurrRun()->getisBlocked())
+            process = getCurrRun();
+    }
+    virtual void addtoterminate(Process* & process){
+          if (getCurrRun()->getisFinished())
+            process = getCurrRun();
+    }
     virtual Process* getNextProcess() = 0;       
     virtual void addToReadyQueue(Process* process) = 0;  // processor or scheduler
     virtual void ScheduleAlgo(int time) = 0;  //new
@@ -28,6 +43,40 @@ private:
     Process* getCurrRun()
     {      //new 
         return CurrRun;
+    }
+    
+    int getRDY_Length() {
+        return RDY_Length;
+    }
+
+    char gettype() {
+        return type;
+    }
+
+    void settype(char a) {
+        type = a;
+    }
+
+    void setCurrRun(Process* p)
+    {     //new
+        CurrRun = p;
+    }
+    Process* getCurrRun()
+    {      //new 
+        return CurrRun;
+    }
+
+    bool isBusy() {
+        if (RDY_Length == 0) return false;
+        else return true;
+    }
+
+    void incrementBusyCount() {
+        if (isBusy()) BusyCount++;
+    }
+
+    int getTotal_TRT() {
+        return Total_TRT;
     }
 
 
