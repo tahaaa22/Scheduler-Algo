58a6c9fa958380fedbb22fd77db832337761e25e
mariammahrous23 <71783484+mariammahrous23@users.noreply.github.com>
2023-04-11 19:16:12 +0200

    Update RR.h

--- Ph1/Project1/RR.h
+++ Ph1/Project1/RR.h
@@ -4,55 +4,64 @@
 class RR : public Processor
 {
 private:
-    Queue<Process*> PQ;
+    Queue<Process*> RdyQueue;
     int TS;    //Timeslice
+    int RTF;
+    int Curtime;
 public:
     RR(int T=0)  
     {
         TS = T;
+        Curtime = 0;
+        settype('R');
     }
     virtual void addToReadyQueue(Process* process)
     {
-        PQ.enqueue(process);
+        RdyQueue.enqueue(process);
     };
     void settimeslice(int t)
     {
         TS = t;
     };
-    //lw msh ha request input/output 
-    //need tt3dal lw htd5ol block fy nos el timeslice yba i need el hba el ht3odhom bs
-    virtual void execute(int &currenttimestep)
+    virtual void ScheduleAlgo(int currenttimestep)
     {
-        //dayman hena el RUN b null;
-        //LW AWL PROCESS FA MAFYA4 HAGA W B3D EL TS BRG3O B NULL
-        //lw mafya4 blocked up time
-        if (PQ.getCount() > 0) // lw while msh ht5rog 5er w kolo terminated
-        {
+        if (!RdyQueue.isEmpty() && !getCurrRun()) {
             Process* temp;         
-            PQ.dequeue(temp);
-            setCurrRun(temp); 
-            int t = temp->gettimeRemaining();
-            if (t - TS > 0) 
-            {
-                _sleep(TS); // msh lazm a sleep bgd sa7?
-                temp->settimeRemaining(t - TS);
-                setCurrRun(NULL);
-                PQ.enqueue(temp);
-                currenttimestep = currenttimestep + TS;
-            }
-            else 
-                // lw fadlha a2a2l mn ts msh ha3od m3tla el program kol dh y3ny  
-            {
-                _sleep(t);
-                temp->settimeRemaining(0);
-                setCurrRun(NULL);
-                getCurrRun()->execute(currenttimestep); // bsgal el terminated time bt3ha
-                addTerminatedProcess(temp);
-                currenttimestep = currenttimestep + t;
-            }
-        
-   
+            RdyQueue.dequeue(temp);
+            setCurrRun(temp);
         }
+
+       /* if (Curtime == 0 && RdyQueue.getCount()>0)
+        {
+            Process* temp;
+            RdyQueue.dequeue(temp);
+            setCurrRun(temp);
+            getCurrRun()->execute(currenttimestep);
+
+        }
+        else if (!getCurrRun())
+        {
+            if (Curtime + 1 == TS) // a5r sec
+            {
+                getCurrRun()->execute(currenttimestep);
+                Curtime = 0;
+                Process* temp= getCurrRun();
+                int t = temp->gettimeRemaining();
+                if (t > 0)
+                {
+                    RdyQueue.enqueue(temp);
+                }
+            }
+            else
+            {
+                getCurrRun()->execute(currenttimestep);
+                Curtime++;
+            }
+        }*/
+            
+            
+            
+       
     };
 
 };
