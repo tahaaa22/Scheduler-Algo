a7b1278012bf9a33f7519dc05a8469a5a403d275
tahaaa22 <128100857+tahaaa22@users.noreply.github.com>
2023-04-15 22:06:45 +0200

    Update UI.h

--- Ph1/Project1/UI.h
+++ Ph1/Project1/UI.h
@@ -2,6 +2,14 @@
 #include <iostream>
 #include <string>
 #include "Queue.h"
+#include <string>
+#include <fstream>
+#include "LinkedList.h"
+#include "Queue.h"
+#include "PriorityQueue.h"
+#include <conio.h>
+#include "Process.h"
+#include "Processor.h"
 using namespace std;
 
 class UI
@@ -15,8 +23,10 @@ public:
 	int ReadMode();
 
 	// Printing the Process Scheduler Status
-	// void Print(); // print function will be repeated for each line in console screen 
-	
+	void Print1(int time, int NF, int NS, int NR, LinkedList<Process*>& ReadyFCFS, Queue<Process*>& ReadyRR, PriorityQueue<Process*>& ReadySJF);
+	void Print2(Queue<Process*> BLKqueue, int BLKcount);
+	void Print3(Queue<Process*> RUNqueue, int runcount, int ID, char type);
+	void Print4(Queue<Process*> TRMqueue, int trmcount);
 	// Silent Mode
 	void printBeforeSim();
 	void printAfterSim();
