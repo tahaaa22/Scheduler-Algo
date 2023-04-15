3cdb2faaf5fa405e42db9b45453192a06b28422f
tahaaa22 <128100857+tahaaa22@users.noreply.github.com>
2023-04-15 22:06:16 +0200

    Update UI.cpp

--- Ph1/Project1/UI.cpp
+++ Ph1/Project1/UI.cpp
@@ -24,9 +24,42 @@ int UI::ReadMode()
 }
 
 //Printing the Process Scheduler Status
- /*void UI::Print()
-{}*/
-
+void UI:: Print1 (int time,int NF,int NS, int NR , LinkedList<Process*>& ReadyFCFS, Queue<Process*>& ReadyRR, PriorityQueue<Process*>& ReadySJF)
+{
+	clear();
+	cout << "Current Timestep:" << time << endl;
+	cout << "--------------------RDY processes--------------------" << endl;
+	cout << "processor 1 [FCFS]:" << NF << "RDY: ";
+	ReadyFCFS.Print();
+	cout << ", " << endl;
+	cout << "processor 2 [SJF]:" << NS << "RDY: " ;
+	ReadySJF.Print();
+	cout << ", " << endl;
+	cout << "processor 3 [RR]:" << NR << "RDY: ";
+	ReadyRR.Print();
+	cout << ", " << endl;
+}
+void UI::Print2(Queue<Process*> BLKqueue, int BLKcount)
+{
+	cout << "--------------------BLK processes--------------------" << endl;
+	cout << BLKcount << "BLK: ";
+	BLKqueue.Print();
+	cout << ", ";
+}
+void UI::Print3(Queue<Process*> RUNqueue, int runcount, int ID, char type)
+{
+	cout << "--------------------RUN processes--------------------" << endl;
+	cout << runcount << "RUN: ";
+	cout << ID<<"(";
+	cout << type << "), ";
+}
+void UI::Print4(Queue<Process*> TRMqueue, int trmcount)
+{
+	cout << "--------------------TRM processes--------------------" << endl;
+	cout << trmcount << "TRM: ";
+	TRMqueue.Print();
+	cout << ", ";
+}
 // Silent Mode
 void UI::printBeforeSim()
 {
