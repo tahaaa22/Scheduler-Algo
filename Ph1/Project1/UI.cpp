e491b89fc49e8f04698f09d4313688e3ce0ea54d
tahaaa22 <128100857+tahaaa22@users.noreply.github.com>
2023-04-17 01:15:43 +0200

    Update UI.cpp

--- Ph1/Project1/UI.cpp
+++ Ph1/Project1/UI.cpp
@@ -1,7 +1,12 @@
 #include "UI.h"
 #include<fstream>
+
 UI::UI()
-{}
+{
+	pf = new FCFS;
+	ps = new SJF;
+	pr= new RR;
+}
 
 void UI::clear()
 {
@@ -23,21 +28,45 @@ int UI::ReadMode()
 	return mode;
 }
 
+
 //Printing the Process Scheduler Status
-void UI:: Print1 (int time,int NF,int NS, int NR , LinkedList<Process*>& ReadyFCFS, Queue<Process*>& ReadyRR, PriorityQueue<Process*>& ReadySJF)
+void UI:: Print1 (int time, int NumProcessor, Processor ** ArrP , int NF, int NS, int NR )
 {
 	clear();
 	cout << "Current Timestep:" << time << endl;
+	cout << "--------------------RDY processes--------------------" << endl;	
+		for (int i = 0; i < NumProcessor; i++)
+		{
+			if (i < NF)
+			{
+				cout << "Processor " << i + 1 << "[FCFS]";
+				pf->ReadyIDs();
+				cout << endl;
+			}
+			else if (i < NF + NS)
+			{
+				cout << "Processor " << i + 1 << "[SJF]";
+				ps->ReadyIDs();
+				cout << endl;
+			}
+			else
+			{
+				cout << "Processor " << i + 1 << "[RR]";
+				pr->ReadyIDs();
+				cout << endl;
+			}
+		}
+	/*cout << "Current Timestep:" << time << endl;
 	cout << "--------------------RDY processes--------------------" << endl;
 	cout << "processor 1 [FCFS]:" << NF << "RDY: ";
-	ReadyFCFS.Print();
+	pf->print_rdy();
 	cout << ", " << endl;
 	cout << "processor 2 [SJF]:" << NS << "RDY: " ;
-	ReadySJF.Print();
+	ps->print_rdy();
 	cout << ", " << endl;
 	cout << "processor 3 [RR]:" << NR << "RDY: ";
-	ReadyRR.Print();
-	cout << ", " << endl;
+	pr->print_rdy();
+	cout << ", " << endl;*/
 }
 void UI::Print2(Queue<Process*> BLKqueue, int BLKcount)
 {
@@ -45,13 +74,27 @@ void UI::Print2(Queue<Process*> BLKqueue, int BLKcount)
 	cout << BLKcount << "BLK: ";
 	BLKqueue.Print();
 	cout << ", ";
+	
 }
-void UI::Print3(Queue<Process*> RUNqueue, int runcount, int ID, char type)
+void UI::Print3(int NumProcessor, Processor** ArrP)
 {
+	int count = 0;
+	for (int i = 0; i < NumProcessor; i++)
+	{
+		if (ArrP[i]->getCurrRun())
+		{
+			count++;
+		}
+	}
+	cout << endl;
 	cout << "--------------------RUN processes--------------------" << endl;
-	cout << runcount << "RUN: ";
-	cout << ID<<"(";
-	cout << type << "), ";
+	cout << count << "RUN: ";
+	for (int i = 0; i < NumProcessor; i++)
+	{
+		if (ArrP[i]->getCurrRun())
+		cout << ArrP[i] ->getCurrRun()->getPID()<< "(P"<<i+1<<"), ";
+	}
+	cout << endl;
 }
 void UI::Print4(Queue<Process*> TRMqueue, int trmcount)
 {
