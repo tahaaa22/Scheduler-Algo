6e41363365007d873ed1f49e704ce41367516ec2
mariammahrous23 <71783484+mariammahrous23@users.noreply.github.com>
2023-04-17 08:48:09 +0200

    Create FCFS.cpp

---
+++ Ph1/Project1/FCFS.cpp
@@ -0,0 +1,12 @@
+#include "FCFS.h"
+
+void FCFS:: Loadp(ifstream& inputFile)
+{
+	inputFile >> MaxW;
+}
+void FCFS:: Loadpf(ifstream& inputFile)
+{
+	inputFile >> forkprob;
+}
+double FCFS::forkprob = 0;
+int FCFS::MaxW = 0;
