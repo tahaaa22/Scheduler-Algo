f3b185a9597a183380ab494b3719b19ca1c88267
mariammahrous23 <71783484+mariammahrous23@users.noreply.github.com>
2023-05-04 13:24:08 +0300

    Update RR.h

--- 8-11_T03_Code/Phase 1-code/Ph1/Project1/RR.h
+++ 8-11_T03_Code/Phase 1-code/Ph1/Project1/RR.h
@@ -29,63 +29,100 @@ public:
     }
 
   
-    virtual void ScheduleAlgo(int time, int  num) // editable in phase 2
+      virtual void ScheduleAlgo(int timestep, int  num)
     {
-        //rdy queue msh empty w mafya4 current bardo
-        if (!RdyQueue.isEmpty() && !getCurrRun()) 
-        {
-            Process* temp;         
-            RdyQueue.dequeue(temp);
-            setCurrRun(temp);
-        }
-        else if (getCurrRun())  //run not empty
-        {
-            if (time <= 15)
-            {
-                Process* P = getCurrRun();
-               P->setisBlocked(true);
-                addtoBLK(P);
-                setCurrRun(nullptr);
-            }
-            else if (time >= 20 && time <= 30)
-            {
-                addToReadyQueue(getCurrRun());
-                setCurrRun(nullptr);
-            }
-            else if (time >= 50 && time <= 60)
-            {
-                Process* P = getCurrRun();
-                addtoterminate( P);
-                setCurrRun(nullptr);
-            }
-        }
-       /* if (Curtime == 0 && RdyQueue.getCount()>0) // for phase 2 stuff related to shceduleralgo
+        //rdy queue msh empty w mafya4 current run bardo
+        if (!RdyQueue.isEmpty() && !getCurrRun())
         {
             Process* temp;
             RdyQueue.dequeue(temp);
             setCurrRun(temp);
-            getCurrRun()->execute(currenttimestep);
-
+            Curtime = 0;
         }
-        else if (!getCurrRun())
+        else if (getCurrRun())  //run not empty
         {
-            if (Curtime + 1 == TS) // a5r sec
+            Process* temp = getCurrRun();
+            int t = temp->gettimeRemaining();
+            if (Curtime == TS && t > 1) // a5r sec fy el ts
             {
-                getCurrRun()->execute(currenttimestep);
+                getCurrRun()->execute(timestep); // bt2alal timestep b 1
                 Curtime = 0;
-                Process* temp= getCurrRun();
-                int t = temp->gettimeRemaining();
-                if (t > 0)
+
+                // BACK TO RDY QUEUE 
+                if (t > 0) // process time slice bt3ha 5las 5ls 
                 {
                     RdyQueue.enqueue(temp);
                 }
+
+                // FROM RUN TO TERMINATION
+                if (t == 0)  
+                {
+                    addtoterminate(temp); // calling fn el schedular
+                }
+
+                // FROM RUN TO BLOCK
+
+                setCurrRun(nullptr);// RUN CURRENTLY EMPTY
             }
             else
             {
-                getCurrRun()->execute(currenttimestep);
+                getCurrRun()->execute(timestep); //a2alal el timestep
                 Curtime++;
+                if (t == 0) // cpu time 5ls 5las bardo 
+                {
+                    addtoterminate(temp); // calling fn el schedular
+                    setCurrRun(nullptr);
+                    Curtime = 0;
+                }
+                // ADDDDD COND BLOCK
+
+
             }
-        }*/
+
+        }
+        ////rdy queue msh empty w mafya4 current bardo
+       //if (!RdyQueue.isEmpty() && !getCurrRun()) 
+       //{
+       //    Process* temp;         
+       //    RdyQueue.dequeue(temp);
+       //    setCurrRun(temp);
+       //}
+       //else if (getCurrRun())  //run not empty
+       //{
+       //    if (time <= 15)
+       //    {
+       //        Process* P = getCurrRun();
+       //       P->setisBlocked(true);
+       //        addtoBLK(P);
+       //        setCurrRun(nullptr);
+       //    }
+       //    else if (time >= 20 && time <= 30)
+       //    {
+       //        addToReadyQueue(getCurrRun());
+       //        setCurrRun(nullptr);
+       //    }
+       //    else if (time >= 50 && time <= 60)
+       //    {
+       //        Process* P = getCurrRun();
+       //        addtoterminate( P);
+       //        setCurrRun(nullptr);
+       //    }
+       //}
+    }
+
+    bool CheckRTF(Process* p1);
+   /* Process* getNextProcess() {
+        if (RdyQueue.isEmpty()) return nullptr;
+        else {
+            Process* temp;
+            RdyQueue.peek(temp);
+            return temp;
+        }
+    }*/
+    virtual void print_rdy()
+    {
+        RdyQueue.Print();
+
     }
 
     virtual  int getRDYCount()
